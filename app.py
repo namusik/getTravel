e07dc1ba78473016972e3fdb65850ee27594b1a6
erionRK621 <jhkim62134@naver.com>
2021-09-13 23:03:38 +0900

    01
    html초안 대충

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
-import requests
 
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('13.125.82.238', 27017, username="test", password="test")
-db = client.dbsparta_plus_week2
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 
+# HTML 화면 보여주기
 @app.route('/')
-def main():
-    return render_template("index.html")
+def home():
+    return render_template('index.html')
+
+
+# API 역할을 하는 부분
+@app.route('/api/mylike', methods=['GET'])
+def show_place():
+    place_like = list(db.myplace.find({'like': 1}, {'_id': False}))
+    return jsonify({'place_likes': place_like})
+
+
+@app.route('/api/like', methods=['POST'])
+def like_star():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'like 연결되었습니다!'})
+
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'delete 연결되었습니다!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
